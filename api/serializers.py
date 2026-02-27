dcfab6535399a3d85ec01e545f3e99ae4c4b3342
vlyd <64774681+vlyd@users.noreply.github.com>
2026-02-27 23:09:14 +0500

    123

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,47 @@
 from rest_framework import serializers
 from .models import Todo
+from django.contrib.auth.models import User
 
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
         fields = '__all__'  # Все поля модели
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+
+    password2 = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "username",
+            "password",
+            "password2",
+            "email"
+        ]
+        extra_kwargs = {
+            "password": {"write_only": True},
+            "email": {"required": True}
+        }
+
+
+    def create(self, validated_data):
+        username = validated_data["username"]
+        email = validated_data["email"]
+        password = validated_data["password"]
+        password2 = validated_data["password2"]
+
+        if password != password2:
+            raise serializers.ValidationError({"password": "Пароли не совпадают"})
+        user = User(username=username)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import TodoViewSet
+from .views import TodoViewSet, RegisterView, ProfileView
 
-router = routers.DefaultRouter()
-router.register(r'todos', TodoViewSet)
+
+#router = routers.DefaultRouter()
+#router.register(r'todos', TodoViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+   # path('', include(router.urls)),
+    path('register/', RegisterView.as_view()),
+    path('profile/', ProfileView.as_view()),
 ]
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
